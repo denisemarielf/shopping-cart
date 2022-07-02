b612554799798938a5eb5613d34271ecba29590e
Denise <denisemarielf@gmail.com>
2022-07-02 13:12:18 -0300

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import "./assets/styles/App.css";
+import Nav from "./components/Nav";
+import { useState } from "react";
+import Homepage from "./components/Homepage";
+import { Routes, Route } from "react-router-dom";
+import Shop from "./components/Shop";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  console.log("rendered");
   return (
     <div className="App">
-      Hello World!
+      <Nav
+        setProducts={setProducts}
+        products={products}
+        quantity={products.length}
+      />
+      <main>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route
+            path="/shop"
+            element={<Shop products={products} setProducts={setProducts} />}
+          />
+        </Routes>
+      </main>
     </div>
   );
 }
