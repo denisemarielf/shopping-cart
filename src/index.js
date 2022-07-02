a1e05159887c004db8eee00e9fb9890455f38ccc
Denise <denisemarielf@gmail.com>
2022-07-02 17:01:45 -0300

    Fix router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={"/app"}>
+    <BrowserRouter basename="/app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
