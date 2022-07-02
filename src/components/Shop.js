956cee3cd07b0dba85dd84c3313e4fca7d508af9
Denise <denisemarielf@gmail.com>
2022-07-02 16:25:33 -0300

    Add categories for products

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -4,12 +4,13 @@ import "../assets/styles/Shop.css";
 
 export default function Shop({ products, setProducts }) {
   const [productsFetched, setProductsFetched] = useState([]);
+  const [category, setCategory] = useState("skincare")
 
   useEffect(() => {
-    fetch("https://dummyjson.com/products/category/skincare")
+    fetch("https://dummyjson.com/products/category/"+category)
       .then((response) => response.json())
       .then((data) => setProductsFetched(data.products));
-  }, []);
+  }, [category]);
 
   const productsDisplay = productsFetched.map((product) => {
     return (
@@ -24,5 +25,26 @@ export default function Shop({ products, setProducts }) {
     );
   });
 
-  return <section className="product-container">{productsDisplay}</section>;
+  return (
+    <section className="shop">
+        <nav className="shop-nav">
+              <h3>Category</h3>
+                <ul className="shop-nav-ul">
+                  <li onClick={() => setCategory("skincare")}>skincare</li>
+                  <li onClick={() => setCategory("fragrances")}>fragrances</li>
+                  <li onClick={() => setCategory("sunglasses")}>sunglasses</li>
+                  <li onClick={() => setCategory("womens-watches")}>watches</li>
+                  <li onClick={() => setCategory("womens-bags")}>bags</li>
+                </ul>
+            </nav>
+            <section className="shop-product-container">
+              {productsDisplay}
+            </section>
+    </section>
+  )
+  
+  
+  
+  
+  
 }
